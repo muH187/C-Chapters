04e4a23e6609687da9716891ff54c0d21ef8eefc
Muhammad Ali <proqualitythings100@gmail.com>
2024-12-04 10:24:48 +0500

    q2

---
+++ Chapter2/q2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+
+    int x, y, z, r, result;
+
+    x = 2, y = 3, z = 3, r = 1;
+
+    result = 3 * x/y - z + r;
+
+    printf("The result of 3 * x/y - z + r: %d", result);
+
+    return 0;
+}
