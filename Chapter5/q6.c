6a7990eb8e8e889e8538ee607f0865a77a5834f9
Muhammad Ali <proqualitythings100@gmail.com>
2024-12-10 10:59:01 +0500

    printing stars

---
+++ Chapter5/q6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void printStars();
+
+int main() {
+
+    printStars();
+
+    return 0;
+}
+
+void printStars()  {
+    printf("*\n");
+    printf("***\n");
+    printf("*****\n");
+}
