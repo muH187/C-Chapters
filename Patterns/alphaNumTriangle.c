9c11826c0cb357912394092eae33a510945e93fd
Muhammad Ali <proqualitythings100@gmail.com>
2025-01-03 15:53:58 +0500

    not yet complete

---
+++ Patterns/alphaNumTriangle.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main() {
+
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    int b = n;
+
+    int a = 1;
+    for(int i = 1; i <=n; i++) {
+        
+        for(int j = 1; j <= i; j++) {
+            int d = a + 64;
+            char ch = (char)d;
+            if(b % 3 == 0) {
+                printf("%d ", j);
+            } else {
+                printf("%c ", ch);
+            }
+            a++;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
